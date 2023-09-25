0a8e52cf84125c6b3ee135dbeffbfd4d703aaa48
PAULO BANDIERA PAIVA <paiva@unifesp.br>
2023-09-25 13:11:57 -0300

    25/9/2023 - Listas encadeadas

--- ED/lista.c
+++ ED/lista.c
@@ -2,21 +2,21 @@
 //
 //
 #include <stdio.h>
+#include <stdlib.h>
 
 struct  listaS {
 	int carga;
 	struct listaS *prox;
-}
-
+};
 
 int main() {
 	int inteiroLido;
 
-	struct ListaS *lista, *novoElemento, *ptr;
+	struct listaS *lista, *novoElemento, *ptr, *ptrAnt;
 
 	lista=NULL;
 
-	printf("\nEntre com inteiros positivos (0 termina)\n")
+	printf("\nEntre com inteiros positivos (0 termina)\n");
 
 	while( 1 ) {
 		printf("\n> ");
@@ -30,8 +30,48 @@ int main() {
 			break;
 		}
 		novoElemento->carga = inteiroLido;
-		novoElemento->prox = lista;
-		lista = novoElemento;     
+		novoElemento->prox = NULL;
+		
+		ptr = lista;
+		if( ptr==NULL ) {
+			lista = novoElemento;
+		}
+		else {
+			while( ptr->prox!=NULL ) {
+				ptr = ptr->prox;
+			}
+			ptr->prox = novoElemento;
+		}
+		
+	}
+	
+// percorre a lista
+	ptr = lista;
+	while( ptr ) {
+		printf("%d\n", ptr->carga );
+		ptr = ptr->prox;
+	}
+
+	printf("\nDeseja remover que elemento: ");
+	scanf("%d", &inteiroLido);
+	
+	ptr = lista;
+	if(ptr->carga==inteiroLido) {
+		lista = lista->prox;
+		free(ptr);
+	}
+	else {
+		while( ptr!=NULL ) {
+			if(ptr->prox==NULL)
+				break;
+			if(ptr->prox->carga==inteiroLido) {
+				ptrAnt=ptr->prox;
+				ptr->prox = ptr->prox->prox;
+				free(ptrAnt);
+				}
+			ptr = ptr->prox;
+		}
+	
 	}
 	
 // percorre a lista
@@ -42,5 +82,7 @@ int main() {
 	}
 
 	return 0;
+	
+	
 }
 
